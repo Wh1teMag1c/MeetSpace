226e624d59bd2924996a1f47fff4c7051028ff56
Dmitry Bartashevskiy <dmitrybwm@yandex.ru>
2026-05-09 23:03:08 +0300

    setup basic routing

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -51,9 +51,10 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'api.User'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -156,3 +157,10 @@ DJOSER = {
         'current_user': 'api.serializers.UserShortSerializer',
     },
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+CORS_ALLOW_CREDENTIALS = True
